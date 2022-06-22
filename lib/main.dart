25ac64dc73102eeee7060d6a3c9d300e732d0ffe
Aida Mumin <aidamuminbusiness@gmail.com>
2022-06-22 13:14:11 -0400

    Commit - 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,33 @@
+//Aida Mumin
+//CSC 4360 - Umoja
+//June 22, 2022
+//Streams of Thoughts
+
+import 'package:streams_of_thoughts/pages/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class Loading extends StatelessWidget {
-  const Loading({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
-  }
-
-}
-
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  final Future<FirebaseApp> _initFirebase = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Social App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: FutureBuilder(
-          initialData: _initFirebase,
-          builder: (BuildContext context, AsyncSnapshot snapshot){
-            if(snapshot.hasError){
-               return const Center(
-                child: Text('Oops'),
-               ); 
-              } else if (snapshot.connectionState == ConnectionState.waiting){
-                return const Loading();
-            }
-            
-            return const Center(
-             child:Text("Good to Go!"),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: const Scaffold(
+        body: 
+          Center(
+            child: Text("Blah"),
+          )
       ),
     );
   }
---
+++ lib/pages/auth.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+//Aida Mumin
+//CSC 4360 - Umoja
+//June 22, 2022
+//Streams of Thoughts
+
+import 'package:flutter/material.dart';
+
+class Auth extends StatelessWidget{
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const Center(child: Text("Welcome to Authentication"));
+  }
+}
