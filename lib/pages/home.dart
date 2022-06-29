6e03b6709c6bb31a96fbf8c83cd3bdd7399a717e
Aida Mumin <aidamuminbusiness@gmail.com>
2022-06-29 02:45:22 -0400

    Commit - 3

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,6 @@ import 'package:flutter/material.dart';
 
 import '../style/style.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -25,37 +24,37 @@ class _HomeState extends State<Home> {
   List<m.User> users = [];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getList();
     getStreamList();
   }
 
-  void getStreamList(){
+  void getStreamList() {
     _db.collection("users").snapshots().listen((snapshots) {
-      for (var element in snapshots.docs) { 
+      for (var element in snapshots.docs) {
         setState(() {
           users.add(m.User.fromJson(element.data()));
         });
       }
-     });
+    });
   }
 
-  void getList(){
-    _db.collection("users").get().then((result) { 
+  void getList() {
+    _db.collection("users").get().then((result) {
       setState(() {
-      for (var element in result.docs) { 
-        users.add(element.data()["name"]);
-      }
-    });
+        for (var element in result.docs) {
+          users.add(element.data()["name"]);
+        }
+      });
     });
   }
 
   void getList2() async {
     var result = await _db.collection("users").get();
-    for (var element in result.docs) { 
-        users.add(element.data()["name"]);
-      }
+    for (var element in result.docs) {
+      users.add(element.data()["name"]);
+    }
   }
 
   @override
@@ -66,11 +65,12 @@ class _HomeState extends State<Home> {
           title: Text(_auth.currentUser!.uid),
         ),
         body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (BuildContext context, int index){
-            return ListTile(title: Text(users[index].name), subtitle: Text(users[index].bio),);
-          })
-        );
+            itemCount: users.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(users[index].name),
+                subtitle: Text(users[index].bio),
+              );
+            }));
   }
-
 }
